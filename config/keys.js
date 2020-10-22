0a87ebc4012a543debaa3490527374d653356c52
Ratnadeep Das Choudhury <63235918+GraniteMask@users.noreply.github.com>
2020-10-23 01:00:24 +0530

    Final Sociogram

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,4 @@
-// module.exports={
-//     MONGOURI: "mongodb+srv://rd:6VxsQWfUuLqwxmSx@cluster0.af9qu.mongodb.net/<dbname>?retryWrites=true&w=majority",
-//     JWT_SECRET:"sdfasasfdcefhouinxakzbbys4"
-// }
 
-//above code was before deployment
 
 //below code is after deployment
 
